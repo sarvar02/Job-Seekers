5e676a4bee3babb41f0e8bd3b2b92e977458282a
AbdulazizkhanKarimbayev <a.k.18072003n.b@gmail.com>
2022-04-27 16:44:54 +0500

    generate job-seekers project

---
+++ src/test/java/uz/isystem/JobSeekers/JobSeekersApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.isystem.JobSeekers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
