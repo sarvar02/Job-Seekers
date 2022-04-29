edf1405247a78b6cb425d059a8ed783e83e484bc
AbdulazizkhanKarimbayev <a.k.18072003n.b@gmail.com>
2022-04-29 15:53:45 +0500

    subject and user manyToMAny connection

--- src/main/java/uz/isystem/JobSeekers/country/CountryDto.java
+++ src/main/java/uz/isystem/JobSeekers/country/CountryDto.java
@@ -3,7 +3,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
