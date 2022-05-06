00d37d33036b4a4ae9867f0781afdcc4f67270bf
AbdulazizkhanKarimbayev <a.k.18072003n.b@gmail.com>
2022-05-06 15:40:13 +0500

    subject and user manyToMAny connection

--- src/main/java/uz/isystem/JobSeekers/user/UserController.java
+++ src/main/java/uz/isystem/JobSeekers/user/UserController.java
@@ -1,28 +1,9 @@
 package uz.isystem.JobSeekers.user;
-
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import uz.isystem.JobSeekers.user.role.Role;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.net.URI;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpStatus.FORBIDDEN;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequiredArgsConstructor
 @RestController
